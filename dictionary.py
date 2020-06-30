58f0f1a7b8c2fa3680f1dac08d1e34dc59db5875
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-06-30 16:59:27 -0400

    init
    start

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open('data.json'))
+
+def wordDef(w):
+	try:
+		w = get_close_matches(w, data.keys(), 1)[0]
+	except:
+		w = ""
+	if w in list(data.keys()):
+		print(w.title()+':'+'\n')
+		return data[w]
+	return "No match found"
+
+
+word = input('Enter a word: ').lower()
+
+print(wordDef(word))
+
+#message to the user bragging bout how dumb/easy their word was haha lol
