c96637af1c4d28c587ca97899adcfedfdc4c9cfc
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-01 21:42:24 -0400

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -2,20 +2,47 @@ import json
 from difflib import get_close_matches
 
 data = json.load(open('data.json'))
+data = dict((k.lower(),v) for k, v in data.items())
 
 def wordDef(w):
-	try:
-		w = get_close_matches(w, data.keys(), 1)[0]
-	except:
-		w = ""
-	if w in list(data.keys()):
-		print(w.title()+':'+'\n')
-		return data[w]
-	return "No match found"
+		if w in list(data.keys()):
+			if len(w) < 5:
+				print('ARE YOU KIDDING!\n')
+			elif len(w) < 8:
+				print('EASY\n')
+			else:
+				print('OK THAT ONE WAS KINDA HARD\n')
+			print(w.title()+':'+'\n')
+			return data[w]
+		try:
+			options = get_close_matches(w, data.keys(), 3)
+			for i in range(len(options)):
+				print(i+1, options[i])
+			w = options[int(input('Did you mean one of these? (type the number next to the correct option)'))-1]
+		except:
+			w = ""
+		if w in list(data.keys()):
+			if len(w) < 5:
+				print('ARE YOU KIDDING!\n')
+			elif len(w) < 8:
+				print('EASY\n')
+			else:
+				print('OK THAT ONE WAS KINDA HARD\n')
+			print(w.title()+':'+'\n')
+			return data[w]
+		return ["No match found"]
 
 
-word = input('Enter a word: ').lower()
-
-print(wordDef(word))
+loop = True
+while loop:
+	loopInput = input('Do you want to use the Dictionary? (Y) or (N)')
+	if loopInput.upper() == 'N':
+		loop = False
+		break
+	word = input('Enter a word: ').lower()
+	answer = wordDef(word)
+	for item in answer:
+		print(item)
+	print('\n')
 
 # message to the user bragging bout how dumb/easy their word was
